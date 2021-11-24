b552b92be32b5986ee957d1150199fea5ddc3adf
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2021-11-24 11:13:16 +0100

    generator sidebar  211124

--- src/components/RosumeGenerator/GeneratorSidebar/GeneratorSidebar.js
+++ src/components/RosumeGenerator/GeneratorSidebar/GeneratorSidebar.js
@@ -1,11 +1,51 @@
-import React, { useState } from 'react'
-import './GeneratorSidebar.css'
+import React, { useState } from "react";
+import "./GeneratorSidebar.css";
+import TextField from "@mui/material/TextField";
 function GeneratorSidebar() {
-    return (
-        <div className="generatorsidebar">
-            <h1>Generator Sidebar</h1>
-        </div>
-    )
+  const [isEdit, setIsEdit] = useState(true);
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+
+  const saveData = () => {
+      setIsEdit(!isEdit)
+  }
+  return (
+    <div className="generatorsidebar">
+      <div className="generatorsidebar__top">
+        {isEdit ?
+          <form onSubmit={saveData}>
+            <div>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Imię i Nazwisko"
+                variant="outlined"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Job"
+                variant="outlined"
+                value={job}
+                onChange={(e) => setJob(e.target.value)}
+              />
+            </div>
+            <button type="submit">Zapisz</button>
+          </form>
+          :
+          <div>
+              <span>{name}</span>
+              <span>{job}</span>
+              <button type="button" onClick={saveData}>Edit</button>
+          </div>
+        }
+      </div>
+    </div>
+  );
 }
 
-export default GeneratorSidebar
+export default GeneratorSidebar;
