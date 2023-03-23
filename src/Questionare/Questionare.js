c5e559d2afce9a16f5a8545f8ca36cb11e92efac
Agnieszka Kaminska <zabula81@o2.pl>
2023-03-23 11:49:47 +0100

    adding responsiveness to the questionare component

--- src/Questionare/Questionare.js
+++ src/Questionare/Questionare.js
@@ -21,7 +21,7 @@ import QuestionareModule from "./QuestionareModule/QuestionareModule";
 import Fieldset from "./Fieldset/Fieldset";
 import UploadImage from "./UploadImage/UploadImage";
 import AddPhotoButton from "./AddPhotoButton/AddPhotoButton";
-import ButtonBack from "../components/Global/ButtonBack/ButtonBack"
+import ButtonBack from "../components/Global/ButtonBack/ButtonBack";
 //img
 import aPhoto from "../assets/images/open-graph.jpg";
 //mui
@@ -206,240 +206,255 @@ function Questionare() {
   return (
     <div className="questionare">
       <ButtonBack />
-      {isEnglish ? <div className="questionare__isEnglish">This page has no translation yet</div> : null}
+      {isEnglish ? (
+        <div className="questionare__isEnglish">
+          This page has no translation yet
+        </div>
+      ) : null}
       <div className="questionare__wrapper">
-      <div className="questionare__header">
-        <img src={aPhoto} loading="lazy" title="web design" alt="web design" />
-        <div>
-          <p>
-            Strony koduję na podstawie doręczonych przez klienta projektów
-            graficznych.
-          </p>
-          <p>
-            Masz projekt a nie potrafisz go zakodować dobrze trafiłeś, zrobię to
-            za Ciebie.
-          </p>
-          <p>
-            Jako frontend developer zajmuję się tylko wyglądem projektu, jako
-            bazy danych używam{" "}
-            <a href="https://firebase.google.com/">firebase</a> - tam również
-            „hostinguję” projekty w czasie realizacji do podglądu dla klienta.
-          </p>
-          <p>
-            Wszystkie niezbędne grafiki (zdjęcia) powinien dostarczyć klient.
-          </p>
-          <p>
-            Na podstawie załączonej ankiety będę w stanie podać Ci cenę usługi.
-            Podchodzę indywidualnie do każdego projektu – nie wszystkie są takie
-            same więc cena jest zależna od oczekiwań klienta.
-          </p>
-          <p>
-            Jeżeli nie chcesz kodowac całej strony a chcesz zmienić tylko jedną
-            rzecz np. nagłówek lub menu nie wypełniaj formularza opisz wszystko
-            w dodatkowych informacjach na dole strony i załącz projekt.
-          </p>
-          <p>
-            Nie masz projektu też możesz napisać - może razem coś wymyślimy{" "}
-            <Emoji label="smile" symbol="😀" />
-          </p>
-          <p>
-            Wycena nie jest zobowiązująca. Nie będą Ci odpowiadać moje warunki
-            po prostu zakończymy wspópracę na tym etapie.
-          </p>
+        <div className="questionare__header">
+          <img
+            src={aPhoto}
+            loading="lazy"
+            title="web design"
+            alt="web design"
+          />
+          <div>
+            <p>
+              Strony koduję na podstawie doręczonych przez klienta projektów
+              graficznych.
+            </p>
+            <p>
+              Masz projekt a nie potrafisz go zakodować dobrze trafiłeś, zrobię
+              to za Ciebie.
+            </p>
+            <p>
+              Jako frontend developer zajmuję się tylko wyglądem projektu, jako
+              bazy danych używam{" "}
+              <a href="https://firebase.google.com/">firebase</a> - tam również
+              „hostinguję” projekty w czasie realizacji do podglądu dla klienta.
+            </p>
+            <p>
+              Wszystkie niezbędne grafiki (zdjęcia) powinien dostarczyć klient.
+            </p>
+            <p>
+              Na podstawie załączonej ankiety będę w stanie podać Ci cenę
+              usługi. Podchodzę indywidualnie do każdego projektu – nie
+              wszystkie są takie same więc cena jest zależna od oczekiwań
+              klienta.
+            </p>
+            <p>
+              Jeżeli nie chcesz kodowac całej strony a chcesz zmienić tylko
+              jedną rzecz np. nagłówek lub menu nie wypełniaj formularza opisz
+              wszystko w dodatkowych informacjach na dole strony i załącz
+              projekt.
+            </p>
+            <p>
+              Nie masz projektu też możesz napisać - może razem coś wymyślimy{" "}
+              <Emoji label="smile" symbol="😀" />
+            </p>
+            <p>
+              Wycena nie jest zobowiązująca. Nie będą Ci odpowiadać moje warunki
+              po prostu zakończymy wspópracę na tym etapie.
+            </p>
+          </div>
         </div>
-      </div>
-      <form className="questionare__form">
-        <QuestionareModule
-          api={apiInfo}
-          checked={checkedApi}
-          setChecked={setCheckedApi}
-          description={
-            <>
-              <p>
-                <strong>Hosting</strong> - udostępnienie miejsca na serwerze
-                (tam wgrywane są pliki żeby były widoczne w sieci) - przy
-                wyborze płatnego hostingu do ceny usługi zostanie doliczona cena
-                hostingu (na rok)
-              </p>
-              <p>
-                <strong>Domena</strong> - adres strony intrenetowej np:{" "}
-                <a
-                  href="https://frontend-agnes.pl"
-                  alt="frontend-agnes.pl"
-                  title="Kodowanie stron internetowych"
-                >
-                  https://frontend-agnes.pl
-                </a>{" "}
-                - do ceny usuługi zostanie doliczona cena domeny (na rok)
-              </p>
-            </>
-          }
-          legend="Informacja o hostingu i domenie"
-        />
-        <QuestionareModule
-          api={functionality}
-          legend="funkcjonalność strony"
-          checked={checkedFunctionality}
-          setChecked={setCheckedFunctionality}
-        />
-        <QuestionareModule
-          api={otherElements}
-          legend="Dodatkowe Elementy"
-          checked={checkedElements}
-          setChecked={setCheckedElements}
-        />
-        <div className="questionare__bottom">
-          <Fieldset legend="Dodatkowe informacje">
-            <textarea
-              className="questionare__textarea"
-              placeholder="dodatkowe informacje..."
-              value={areaField}
-              onChange={(e) => setAreaField(e.target.value)}
-            />
-          </Fieldset>
-        </div>
-        <div className="questionare__uploadImage uploadImage">
-          <Fieldset legend="Dodaj projekt graficzny strony">
-            <div className="questionare__wrapper">
-              <UploadImage
-                progress={progress}
-                preview={preview}
-                setPreview={setPreview}
-                image={image}
-                setImage={setImage}
+        <form className="questionare__form">
+          <QuestionareModule
+            api={apiInfo}
+            checked={checkedApi}
+            setChecked={setCheckedApi}
+            description={
+              <>
+                <p>
+                  <strong>Hosting</strong> - udostępnienie miejsca na serwerze
+                  (tam wgrywane są pliki żeby były widoczne w sieci) - przy
+                  wyborze płatnego hostingu do ceny usługi zostanie doliczona
+                  cena hostingu (na rok)
+                </p>
+                <p>
+                  <strong>Domena</strong> - adres strony intrenetowej np:{" "}
+                  <a
+                    href="https://frontend-agnes.pl"
+                    alt="frontend-agnes.pl"
+                    title="Kodowanie stron internetowych"
+                  >
+                    https://frontend-agnes.pl
+                  </a>{" "}
+                  - do ceny usuługi zostanie doliczona cena domeny (na rok)
+                </p>
+              </>
+            }
+            legend="Informacja o hostingu i domenie"
+          />
+          <QuestionareModule
+            api={functionality}
+            legend="funkcjonalność strony"
+            checked={checkedFunctionality}
+            setChecked={setCheckedFunctionality}
+          />
+          <QuestionareModule
+            api={otherElements}
+            legend="Dodatkowe Elementy"
+            checked={checkedElements}
+            setChecked={setCheckedElements}
+          />
+          <div className="questionare__bottom">
+            <Fieldset legend="Dodatkowe informacje">
+              <textarea
+                className="questionare__textarea"
+                placeholder="dodatkowe informacje..."
+                value={areaField}
+                onChange={(e) => setAreaField(e.target.value)}
               />
-              <div className="questionare__buttonsGroup">
-                {!imageVisibleOne ? (
-                  <AddCardIcon
-                    sx={{
-                      cursor: "pointer",
-                      fontSize: "36px",
-                      color: "#008000",
-                    }}
-                    onClick={() => setImageVisibleOne(true)}
-                  />
-                ) : null}
-                <AddPhotoButton
-                  approvePhoto={() => approvePhoto(image, setProgress)}
-                  image={image}
-                  progress={progress}
-                  preview={preview}
-                  setProgress={setProgress}
-                />
-              </div>
-            </div>
-            {imageVisibleOne ? (
+            </Fieldset>
+          </div>
+          <div className="questionare__uploadImage uploadImage">
+            <Fieldset legend="Dodaj projekt graficzny strony">
               <div className="questionare__wrapper">
                 <UploadImage
-                  progress={progressOne}
-                  preview={previewOne}
-                  setPreview={setPreviewOne}
-                  image={imageOne}
-                  setImage={setImageOne}
+                  progress={progress}
+                  preview={preview}
+                  setPreview={setPreview}
+                  image={image}
+                  setImage={setImage}
                 />
                 <div className="questionare__buttonsGroup">
-                  {!imageVisibleTwo ? (
+                  {!imageVisibleOne ? (
                     <AddCardIcon
                       sx={{
                         cursor: "pointer",
                         fontSize: "36px",
-                        color: "#008022",
+                        color: "#008000",
                       }}
-                      onClick={() => setImageVisibleTwo(true)}
-                    />
-                  ) : null}
-                  {progressOne < 100 ? (
-                    <RemoveCircleIcon
-                      onClick={() => setImageVisibleOne(false)}
-                      sx={{
-                        cursor: "pointer",
-                        fontSize: "36px",
-                        color: "#ff0000",
-                      }}
+                      onClick={() => setImageVisibleOne(true)}
                     />
                   ) : null}
                   <AddPhotoButton
-                    approvePhoto={() => approvePhoto(imageOne, setProgressOne)}
-                    image={imageOne}
+                    approvePhoto={() => approvePhoto(image, setProgress)}
+                    image={image}
+                    progress={progress}
+                    preview={preview}
+                    setProgress={setProgress}
+                  />
+                </div>
+              </div>
+              {imageVisibleOne ? (
+                <div className="questionare__wrapper">
+                  <UploadImage
                     progress={progressOne}
                     preview={previewOne}
-                    setProgress={setProgressOne}
+                    setPreview={setPreviewOne}
+                    image={imageOne}
+                    setImage={setImageOne}
                   />
-                </div>
-              </div>
-            ) : null}
-            {imageVisibleTwo ? (
-              <div className="questionare__wrapper">
-                <UploadImage
-                  progress={progressTwo}
-                  preview={previewTwo}
-                  imageVivibleR={imageVisibleTwo}
-                  setImageVisibleR={setImageVisibleTwo}
-                  setPreview={setPreviewTwo}
-                  image={imageTwo}
-                  setImage={setImageTwo}
-                />
-                <div className="questionare__buttonsGroup">
-                  {progressTwo < 100 ? (
-                    <RemoveCircleIcon
-                      onClick={() => setImageVisibleTwo(false)}
-                      sx={{
-                        cursor: "pointer",
-                        fontSize: "36px",
-                        color: "#ff0000",
-                      }}
+                  <div className="questionare__buttonsGroup">
+                    {!imageVisibleTwo ? (
+                      <AddCardIcon
+                        sx={{
+                          cursor: "pointer",
+                          fontSize: "36px",
+                          color: "#008022",
+                        }}
+                        onClick={() => setImageVisibleTwo(true)}
+                      />
+                    ) : null}
+                    {progressOne < 100 ? (
+                      <RemoveCircleIcon
+                        onClick={() => setImageVisibleOne(false)}
+                        sx={{
+                          cursor: "pointer",
+                          fontSize: "36px",
+                          color: "#ff0000",
+                        }}
+                      />
+                    ) : null}
+                    <AddPhotoButton
+                      approvePhoto={() =>
+                        approvePhoto(imageOne, setProgressOne)
+                      }
+                      image={imageOne}
+                      progress={progressOne}
+                      preview={previewOne}
+                      setProgress={setProgressOne}
                     />
-                  ) : null}
-                  <AddPhotoButton
-                    approvePhoto={() => approvePhoto(imageTwo, setProgressTwo)}
-                    image={imageTwo}
+                  </div>
+                </div>
+              ) : null}
+              {imageVisibleTwo ? (
+                <div className="questionare__wrapper">
+                  <UploadImage
                     progress={progressTwo}
                     preview={previewTwo}
-                    setProgress={setProgressTwo}
+                    imageVivibleR={imageVisibleTwo}
+                    setImageVisibleR={setImageVisibleTwo}
+                    setPreview={setPreviewTwo}
+                    image={imageTwo}
+                    setImage={setImageTwo}
                   />
+                  <div className="questionare__buttonsGroup">
+                    {progressTwo < 100 ? (
+                      <RemoveCircleIcon
+                        onClick={() => setImageVisibleTwo(false)}
+                        sx={{
+                          cursor: "pointer",
+                          fontSize: "36px",
+                          color: "#ff0000",
+                        }}
+                      />
+                    ) : null}
+                    <AddPhotoButton
+                      approvePhoto={() =>
+                        approvePhoto(imageTwo, setProgressTwo)
+                      }
+                      image={imageTwo}
+                      progress={progressTwo}
+                      preview={previewTwo}
+                      setProgress={setProgressTwo}
+                    />
+                  </div>
                 </div>
+              ) : null}
+            </Fieldset>
+          </div>
+          <div className="questionare__adress">
+            <Fieldset legend="Podaj dane do kontaktu">
+              <div className="questionare__input">
+                <TextField
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  label="Podaj Imię"
+                  variant="standard"
+                  fullWidth
+                />
               </div>
-            ) : null}
-          </Fieldset>
-        </div>
-        <div className="questionare__adress">
-          <Fieldset legend="Podaj dane do kontaktu">
-            <div className="questionare__input">
-              <TextField
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                label="Podaj Imię"
-                variant="standard"
-                fullWidth
-              />
-            </div>
-            <div className="questionare__input">
-              <TextField
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                label="Podaj email"
-                variant="standard"
-                fullWidth
-              />
-            </div>
-          </Fieldset>
-        </div>
-        <input
-          type="text"
-          placeholder="age"
-          name="age"
-          autoComplete="off"
-          className="questionare__age"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-        <div className="questionere__button">
-          <FormButton type="button" onClick={formHandler}>
-            Wyślij
-          </FormButton>
-        </div>
-      </form>
-    </div>
+              <div className="questionare__input">
+                <TextField
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  label="Podaj email"
+                  variant="standard"
+                  fullWidth
+                />
+              </div>
+            </Fieldset>
+          </div>
+          <input
+            type="text"
+            placeholder="age"
+            name="age"
+            autoComplete="off"
+            className="questionare__age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <div className="questionere__button">
+            <FormButton type="button" onClick={formHandler}>
+              Wyślij
+            </FormButton>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
